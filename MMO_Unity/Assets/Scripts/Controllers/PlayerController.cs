4b7761d6bf2fb25a030f56889cf6b6a2fd07859f
Jihoon Song <spec0003@naver.com>
2022-10-03 08:16:24 +0900

    8.9. UI Manager #2

--- MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
+++ MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
@@ -14,10 +14,7 @@ public class PlayerController : MonoBehaviour
 		Managers.Input.MouseAction -= OnMouseClicked;
 		Managers.Input.MouseAction += OnMouseClicked;
 
-		// TEMP
-		UI_Button ui = Managers.UI.ShowPopupUI<UI_Button>();
-		Managers.UI.ClosePopupUI(ui);
-		//Managers.UI.ClosePopupUI();
+		Managers.UI.ShowPopupUI<UI_Button>();
 	}
 
 	public enum PlayerState
