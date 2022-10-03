d20d3e99fac0dff7fdef0f7a4c5f963b33624c0f
Jihoon Song <spec0003@naver.com>
2022-10-03 17:53:20 +0900

    8.11. 인벤토리 실습 #2

--- MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
+++ MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
@@ -14,7 +14,8 @@ public class PlayerController : MonoBehaviour
 		Managers.Input.MouseAction -= OnMouseClicked;
 		Managers.Input.MouseAction += OnMouseClicked;
 
-		Managers.UI.ShowPopupUI<UI_Button>();
+		// TEMP
+		Managers.UI.ShowSceneUI<UI_Inven>();
 	}
 
 	public enum PlayerState
